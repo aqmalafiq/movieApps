fc7369aafcc18c011816320dba8f4401bdddb7b0
aqmalafiq <aqmal@apptivitylab.com>
2017-05-16 08:58:08 +0800

    Initial Commit

---
+++ movieApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movieApplication
+//
+//  Created by Aqmal on 16/05/2017.
+//  Copyright © 2017 Aqmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
